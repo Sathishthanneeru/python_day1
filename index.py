3fd0eeb757d4aab83671d93cc1bc3b490226ecb7
Sathishthanneeru <sathishthanneeru36@gmail.com>
2025-02-01 23:22:11 +0530

    new changes

---
+++ index.py
@@ -0,0 +1,105 @@
+# num1 = float(input("enter the frist number:"))
+
+# if num1 > 0:
+#     print("number is postive")
+# elif num1 == 0:
+#     print("enter number zero")
+# else :
+#     print("entre number is negitive")
+
+
+
+# num1=int(input("enter a number:"))
+# if num1 %2 == 0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+
+
+# age=int(input("enter your age:"))
+# if age >= 18:
+#     print("you are eligible fot voting")
+# else:
+#     print("not eligiable for voting")
+
+
+
+# marks=float(input("enter your marks:"))
+# if marks > 40:
+#     print("PASS")
+# else:
+#     print("FAIL")
+
+
+
+# num1=float(input("enter a number from 0 to 6:"))
+# if num1 == 0:
+#     print("sunday")
+# elif num1 == 1:
+#     print("monday")
+# elif num1 == 2:
+#     print("tuesday")
+# elif num1 == 3:
+#     print("wednessday")
+# elif num1 == 4:
+#     print("thursday")
+# elif num1 == 5:
+#     print("friday")
+# elif num1 == 6:
+#     print("saturday")
+# else:
+#     print("enter the correct number")
+
+
+
+
+# operator=input("enter the operator name:")
+# num1=int(input("enter frist number:"))
+# num2=int(input("enter second number"))
+# if add=num1+num2
+
+
+
+# num1 = float(input("enter frist number:"))
+# num2 = float(input("enter second number:"))
+# num3 = float(input("enter third number:"))
+# if num1>num2 and num1>num3:
+#     greatest=num1
+# elif num2>num3 and num2>num1:
+#     greatest=num2
+# else :
+#     greatest=num3
+# print("the gretest number is:",greatest)
+
+
+
+# marks=float(input("enter the marks:"))
+# if marks >= 90 and marks <= 100:
+#     print("A grade")
+# elif marks >= 80 and marks < 90:
+#     print("B grade")
+# elif marks >= 70 and marks < 80:  
+#     print("C grade")
+# else:
+#     print("fail")
+
+
+
+# year=int(input("enter the year:"))
+# if year%4==0 and year %100!=0 or year%400==0:
+#     print("is a leap uear")
+# else:
+#     print("is not a leap year")
+
+
+
+
+
+
+
+
+
+
+
+
