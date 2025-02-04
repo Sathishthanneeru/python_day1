7b51c6c875566c8cb3a69307f2da57b900b0cfe8
Sathishthanneeru <sathishthanneeru36@gmail.com>
2025-02-04 12:12:40 +0530

    new changes

--- index.py
+++ index.py
@@ -84,10 +84,59 @@
 
 
 # year=int(input("enter the year:"))
-# if year%4==0 and year %100!=0 or year%400==0:
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
 #     print("is a leap uear")
 # else:
 #     print("is not a leap year")
+
+
+
+
+# using terinary operators
+
+# year='is a leap year' if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 else 'not aleap yeear'
+# print(year)
+
+
+
+
+# side1=float(input("side1:"))
+# side2=float(input("side2:"))
+# side3=float(input("side3:"))
+# print("traingle is possible" if (side1+side2>side3) and (side2+side3>side1) and (side1+side3>side2) else "it is not possible")
+
+
+
+
+
+
+# by using string to find vowels, consonants,neither
+
+# str1=input("enter a single character:").lower()
+# if len(str1)>1 or len(str1)==0:
+#     print("i cant run the code")
+# else:
+#     if str1 in "aeiou":
+#         print("vowels")
+#     elif str1.isalpha():
+#         print("consonants")
+#     else:
+#         print("neither")
+
+
+
+
+
+# by using list
+
+# str2=input("enter a single character:").lower()
+# if str2 in['a','e','i','o','u']:
+#     print("vowels")
+# else:
+#     print("consonants")
+ 
+
+
 
 
 
