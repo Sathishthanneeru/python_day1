1d38d148835f0c5338114efb6ff25001210f1506
Sathishthanneeru <sathishthanneeru36@gmail.com>
2025-02-14 11:28:25 +0530

    new changes

---
+++ functions.py
@@ -0,0 +1,35 @@
+# def even_odd(x):
+#     if x%2==0:
+#        return ("even")
+#     else:
+#         return ("odd")
+# print(even_odd(2))
+
+
+# def voters_eligibility(age):w
+#     if age>=18:
+#         return True
+#     else:
+#         return False
+# print(voters_eligibility(2))
+    
+
+
+
+
+
+
+
+# def chec_prime(num1):
+#     temp=num1
+#     res=0
+#     while temp>0:
+#         rem=temp%10
+#         res+=rem**len(str(num1))
+#         temp=temp//10
+#     return res==num1
+# lower_bond=int(input("entre the lower bond"))
+# upper_bond=int(input("enter the upper bond"))
+# for i in range(lower_bond,upper_bond+1):
+#     if chec_prime(i):
+#         print(i)
---
+++ loops.py
@@ -0,0 +1,153 @@
+# for i in range (0,101,3):
+#         print(i)
+
+
+# for i in range (1,101,2):
+#         print(i)
+
+
+# for i in range (0,101,2):
+#         print(i)
+
+
+
+# for i in range(0,101,5):
+#     # if i%3==0:
+#         print(5,'x',i//5,'=',i)
+
+
+# sum=0
+# for i in range(1,101):
+#     sum = sum + i
+# print(sum)
+        
+
+# n=20
+# print ((n*(n+1))/2)
+
+
+# table=int(input("enter a number: "))
+# for i in range(1,21):
+#     print(table,"X",i,"=",table*i)
+
+
+
+# num_reverse=int(input("enter a number: "))
+# s=0
+# rev=0
+# while(num_reverse):
+#     res=num_reverse%10
+#     s+=res
+#     rev=rev*10+res
+#     num_reverse//=10
+# print(s,"sum of all digits ")
+# print(rev)
+
+
+# num_count=int(input("enter a nuber: "))
+# count=0
+# while(num_count):
+#     count+=1
+#     num_count//=10
+# print("total numbers are:",count)
+
+
+
+
+# num=1
+# while(num>=0):
+#     num=int(input("enter a number:")) 
+# print("u enter negitive number")
+
+
+
+# a=0
+# b=1
+# print(a,b,end=",")
+# for i in range(1,20):
+#     c=a+bz
+#     a=b
+#     b=c
+#     print(c,end=",")
+
+
+# num1=int(input("enter a number:"))
+# if num1<1:
+#     print(num1,"is not a prime number")
+# else:
+#     count=0
+#     for i in range(2,num1):
+#         if num1%i==0:
+#             count+=1
+#     if count:
+#         print(num1,"number is not a prime")
+#     else:
+#         print(num1,"number is prime")
+
+  
+
+
+# fact_num=int(input("enter a number:"))
+# fact=1
+# for i in range(1,fact_num+1):
+#     fact*=i
+# print(fact)
+
+
+
+# for i in range(1,101):
+#     print(i,end=",")if i%3==0 or i%5==0 else print(end="")
+
+
+
+# simple format
+
+
+# for i in range(1,51):
+#     if i%3==0 or i%5==0:
+#         print(i,end=",")
+
+
+
+
+
+# num1=int(input("enter a number:"))
+# print("press 1 for square of number")
+# print("press 2 for cube of a number")
+# print("press 3 for exit")
+# a=int(input("enter a number"))
+# if a==1:
+#     print(num1**2)
+# elif a==2:
+#     print(num1**3)
+# elif a==3:
+#     print("exit")
+# else:
+#     print("invlaid response")
+
+
+
+# user_name="sathish@123"
+# password="123456789"
+# count=0
+# for i in range(3):
+#     user=input("enter the user name:")
+#     p=input("enter the password:")
+#     if user_name==user and password==p:
+#         print("login is successfully!!!!")
+#     elif password==p:
+#         print("password is correct user name is wrong")
+
+#         break
+#     elif count<2:
+#         print("try again")
+#         count+=1
+# if count:
+#     print("your account is blocked for 24 hours")
+
+
+
+
+
+        
+
